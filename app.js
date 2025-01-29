6815f39b7d565dca3cfc2f2818bc2709b57cd07e
Vitor Oliveira <vitor.oliveira@digitalscutum.com.br>
2025-01-28 21:17:04 -0300

    Implementação do metodo adicionarAmigo

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
-let listaDeNomes = [];
 
+const nomeInput = document.getElementById("amigo");
+let listaHTML = document.getElementById("listaAmigos");
 
-function adicionarAmigo(){
+function adicionarAmigo() {
+    let nome = nomeInput.value;
+    console.log(nome);
 
+    if (nome === "") {
+        alert("Por favor, insira um nome válido.");
+        return;
+    }
+    const itensLista = Array.from(listaHTML.children).map((item) => item.textContent);
+
+    if (itensLista.includes(nome)) {
+        alert("Este nome já foi adicionado.");
+        return;
+    }
+    // Cria um novo elemento <li> para adicionar o nome
+    const novoItem = document.createElement("li");
+    novoItem.textContent = nome;
+
+    // Adiciona o <li> na lista UL
+    listaHTML.appendChild(novoItem);
+
+    // Limpa o campo de input
+    nomeInput.value = "";
+    console.log(listaHTML.innerHTML);
 }
 
-function sortearAmigo(){
 
-}
+// function sortearAmigo() {
+//     let listaResultado = document.getElementById("resultado");
+//
+// }
